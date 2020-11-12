c2c87b4ac870f4ee51c711b96e9a6d0a6950e8ab
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-11-12 15:25:42 +0900

    teachersテーブルとstudentsテーブルの作成

--- db/migrate/20201112055119_devise_create_teachers.rb
+++ db/migrate/20201112055119_devise_create_teachers.rb
@@ -4,8 +4,15 @@ class DeviseCreateTeachers < ActiveRecord::Migration[6.0]
   def change
     create_table :teachers do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.integer :employee_number,    null: false
+      t.integer :status_id,          null: false
+      t.string  :last_name,          null: false
+      t.string  :first_name,         null: false
+      t.string  :last_name_h,        null: false
+      t.string  :first_name_h,       null: false
+      t.string  :email,              null: false, default: ""
+      t.string  :encrypted_password, null: false, default: ""
+      t.string  :nickname,           null: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/migrate/20201112055209_devise_create_students.rb
+++ db/migrate/20201112055209_devise_create_students.rb
@@ -4,8 +4,15 @@ class DeviseCreateStudents < ActiveRecord::Migration[6.0]
   def change
     create_table :students do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.integer :student_number,     null: false
+      t.string  :last_name,          null: false
+      t.string  :first_name,         null: false
+      t.string  :last_name_h,        null: false
+      t.string  :first_name_h,       null: false
+      t.string  :nickname,           null: false
+      t.string  :email,              null: false, default: ""
+      t.string  :encrypted_password, null: false, default: ""
+      t.date    :birthday,           null: false
 
       ## Recoverable
       t.string   :reset_password_token
