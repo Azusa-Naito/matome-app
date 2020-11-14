fe6e1401973e4f8619e5dd39232827e2f8e84f01
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-11-14 16:34:42 +0900

    TeacherとStudentの新規登録画面View作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :students
   devise_for :teachers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "classrooms#index"
 end
