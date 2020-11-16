47ee04280b434cdb83b403c1a764f8d1f140839d
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-11-16 23:53:10 +0900

    classrooms_controller記述追加

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,2 +1,25 @@
 class ClassroomsController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @classroom = Classroom.new
+  end
+
+  def create
+    @classroom = Classroom.new(classroom_params)
+    if @classroom.valid?
+      @classroom.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def classroom_params
+    params.require(:classroom).permit(:name, :year_month)
+  end
 end
