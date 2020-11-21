9ce810af5045fe7f3b2fd456f90971cad1ce8d42
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-11-21 17:37:50 +0900

    担任クラスの表示（View）

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,6 +1,9 @@
 class ClassroomsController < ApplicationController
   def index
     @classrooms = Classroom.all
+    if teacher_signed_in?
+      @classrooms_hr = Classroom.where(teacher_id: current_teacher.id)
+    end
   end
 
   def new
@@ -26,6 +29,6 @@ class ClassroomsController < ApplicationController
 
   def classroom_params
     # 中間テーブルのレコードは一つずつ作成しているため、idsとはならない
-    params.require(:classroom).permit(:name, :year_month, classroom_teachers_attributes: [:id, :teacher_id, :room_id, :_destroy], classroom_students_attributes: [:id, :student_id, :room_id, :_destroy])
+    params.require(:classroom).permit(:name, :year_month, classroom_teachers_attributes: [:id, :teacher_id, :room_id, :_destroy], classroom_students_attributes: [:id, :student_id, :room_id, :_destroy]).merge(teacher_id: current_teacher.id)
   end
 end
