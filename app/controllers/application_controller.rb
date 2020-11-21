609352a147393443eaa9c26ac469715535c4e1ac
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-11-21 14:49:56 +0900

    devise_parameter_sanitizerメソッドの定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:employee_number, :status_id, :last_name, :first_name, :last_name_k, :first_name_k, :image, :nickname, :student_number, :name, :name_k, :birthday])
+  end
 end
+
