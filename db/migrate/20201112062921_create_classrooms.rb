5fb5f0685c87f3b11ccca52d7893d584718e43f3
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-11-21 17:37:00 +0900

    classroomsテーブルの変更
    担任（クラスルーム作成者）を保存できるように変更

--- db/migrate/20201112062921_create_classrooms.rb
+++ db/migrate/20201112062921_create_classrooms.rb
@@ -3,6 +3,7 @@ class CreateClassrooms < ActiveRecord::Migration[6.0]
     create_table :classrooms do |t|
       t.string     :name,       null: false
       t.date       :year_month, null: false
+      t.references :teacher,    foreign_key: true
       t.timestamps
     end
   end
