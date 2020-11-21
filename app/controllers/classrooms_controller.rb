fc19a3e11dc90311c4230aca8406f3e52c09eaca
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-11-21 14:49:09 +0900

    classroom_controller記述の追加

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,10 +1,12 @@
 class ClassroomsController < ApplicationController
   def index
-
+    @classrooms = Classroom.all
   end
 
   def new
     @classroom = Classroom.new
+    # @classroom_teacher = @classroom.classroom_teachers.build
+    # @classroom_student = @classroom.classroom_students.build
   end
 
   def create
@@ -17,9 +19,13 @@ class ClassroomsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
 
   def classroom_params
-    params.require(:classroom).permit(:name, :year_month)
+    # 中間テーブルのレコードは一つずつ作成しているため、idsとはならない
+    params.require(:classroom).permit(:name, :year_month, classroom_teachers_attributes: [:id, :teacher_id, :room_id, :_destroy], classroom_students_attributes: [:id, :student_id, :room_id, :_destroy])
   end
 end
