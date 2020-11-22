cbbf1debab8e37e0e434dfa7eb52a4c327ca19fe
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-11-22 18:03:42 +0900

    informationとのアソシエーション記述

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,2 +1,7 @@
 class Information < ApplicationRecord
+  # アソシエーション
+  belongs_to :teacher
+  # ActiveHashのメソッド
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :genre
 end
