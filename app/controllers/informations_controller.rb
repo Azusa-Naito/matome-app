eef4a9cfc8ef20100e57ce8def85022bc3f5aeb2
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-11-22 18:05:54 +0900

    コントローラー記述

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -1,2 +1,29 @@
 class InformationsController < ApplicationController
+  def index
+    @informations = Information.all
+  end
+
+  def new
+    @information = Information.new
+  end
+
+  def create
+    @information = Information.new(information_params)
+    if @information.valid?
+      @information.save
+      redirect_to root_path
+    else
+      render :new 
+    end
+  end
+
+  def show
+    @information = Information.find(params[:id])
+  end
+
+  private
+
+  def information_params
+    params.require(:information).permit(:title, :genre_id, :content).merge(teacher_id: current_teacher.id)
+  end
 end
