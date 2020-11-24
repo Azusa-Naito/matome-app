76d8177edbfbca7ce54c088cd96fe3f3e5f0a0c0
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-11-24 17:35:30 +0900

    アソシエーションとバリデーションの記述

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,2 +1,6 @@
 class Chat < ApplicationRecord
+  # アソシエーション
+  belongs_to :classroom
+  # バリデーション
+  validates :text, presence: true
 end
--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,7 +1,8 @@
 class Classroom < ApplicationRecord
-  # TeachersテーブルとStudentsテーブルとのアソシエーション
+  # TeachersテーブルとStudentsテーブルとChatsテーブルとのアソシエーション
   has_many :teachers, through: :classroom_teachers
   has_many :students, through: :classroom_students
+  has_many :chats
   # 中間テーブルとのアソシエーション
   has_many :classroom_teachers, dependent: :destroy
   has_many :classroom_students, dependent: :destroy
