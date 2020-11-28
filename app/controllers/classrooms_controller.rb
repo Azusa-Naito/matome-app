bb160393e1709e53e28b04151ee34bbb8c46536c
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-11-28 21:00:48 +0900

    学生ログイン時表示のビュー作成

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,8 +1,13 @@
 class ClassroomsController < ApplicationController
   def index
-    @classrooms = Classroom.all
     if teacher_signed_in?
+      # 担任クラス
       @classrooms_hr = Classroom.where(teacher_id: current_teacher.id)
+      # 授業だけのクラス
+      @classrooms_cl = current_teacher.classrooms
+      @classrooms = Classroom.all
+    elsif student_signed_in?
+      @classrooms_st = current_student.classrooms
     end
   end
 
