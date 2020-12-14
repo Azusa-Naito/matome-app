1010d93ad7cf408b5209b70c31425bd971e2bb3d
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-12-15 07:58:13 +0900

    コントローラーの記述

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,2 +1,30 @@
 class SubmissionsController < ApplicationController
+
+  def new
+    @submission = Submission.new
+  end
+
+  def create
+    @classroom = Classroom.find(params[:classroom_id])
+    @homework = Homework.find(params[:homework_id])
+    @submission = Submission.new(homework_params)
+    if @submission.valid?
+      @submission.save
+      redirect_to classroom_homework_path(@classroom.id, @homework.id)
+    else
+      render "submissions/new"
+    end
+  end
+
+  def show
+    @submission = submission.find(params[:id])
+  end
+
+
+  private
+
+  def homework_params
+    params.require(:submission).permit(:content, :file).merge(homework_id: @homework.id, student_id: current_student.id)
+  end
+  
 end
