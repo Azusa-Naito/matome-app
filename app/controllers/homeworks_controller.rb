cdd2344d734194dd86a21269939dd45248792350
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-12-18 15:29:22 +0900

    宿題と宿題提出に編集・更新・削除機能追加

--- app/controllers/homeworks_controller.rb
+++ app/controllers/homeworks_controller.rb
@@ -1,12 +1,13 @@
 class HomeworksController < ApplicationController
-  before_action :authenticate_teacher!, only: [:new, :create]
+  before_action :authenticate_teacher!, except: [:show]
+  before_action :set_classroom, only: [:create, :show, :update, :destroy]
+  before_action :set_homework, only: [:show, :edit, :update]
   
   def new
     @homework = Homework.new
   end
 
   def create
-    @classroom = Classroom.find(params[:classroom_id])
     @homework = Homework.new(homework_params)
     if @homework.valid?
       @homework.save
@@ -17,13 +18,42 @@ class HomeworksController < ApplicationController
   end
 
   def show
-    @homework = Homework.find(params[:id])
   end
 
+  def edit
+    if current_teacher.id != @homework.teacher_id 
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @homework.update(homework_params)
+      redirect_to classroom_path(@classroom.id)
+    else
+      render "homeworks/edit"
+    end
+  end
+
+  def destroy
+    homework = Homework.find(params[:id])
+    if homework.destroy
+      redirect_to classroom_path(@classroom.id)
+    else
+      render "homeworks/show"
+    end
+  end
 
   private
 
   def homework_params
-    params.require(:homework).permit(:start_time, :content, :file, :judgement).merge(classroom_id: @classroom.id, teacher_id: current_teacher.id)
+    params.require(:homework).permit(:start_time, :content, :judgement, :file).merge(classroom_id: @classroom.id, teacher_id: current_teacher.id)
+  end
+
+  def set_classroom
+    @classroom = Classroom.find(params[:classroom_id])
+  end
+
+  def set_homework
+    @homework = Homework.find(params[:id])
   end
 end
--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,13 +1,15 @@
 class SubmissionsController < ApplicationController
+  before_action :set_classroom, only: [:create, :show, :update, :destroy]
+  before_action :set_homework, only: [:create, :show, :update]
+  before_action :set_submission, only: [:show, :edit, :update]
+
 
   def new
     @submission = Submission.new
   end
 
   def create
-    @classroom = Classroom.find(params[:classroom_id])
-    @homework = Homework.find(params[:homework_id])
-    @submission = Submission.new(homework_params)
+    @submission = Submission.new(submission_params)
     if @submission.valid?
       @submission.save
       redirect_to classroom_homework_path(@classroom.id, @homework.id)
@@ -17,14 +19,47 @@ class SubmissionsController < ApplicationController
   end
 
   def show
-    @submission = submission.find(params[:id])
+    @student = Student.find(@submission.student_id)
   end
 
+  def edit
+    if current_student.id != @submission.student_id 
+      redirect_to classroom_path(@classroom.id)
+    end
+  end
+
+  def update
+    if @submission.update(submission_params)
+      redirect_to classroom_path(@classroom.id)
+    else
+      render "submissions/edit"
+    end
+  end
+
+  def destroy
+    submission = Submission.find(params[:id])
+    if submission.destroy
+      redirect_to classroom_path(@classroom.id)
+    else
+      render "submissions/show"
+    end
+  end
 
   private
 
-  def homework_params
+  def submission_params
     params.require(:submission).permit(:content, :file).merge(homework_id: @homework.id, student_id: current_student.id)
   end
   
+  def set_classroom
+    @classroom = Classroom.find(params[:classroom_id])
+  end
+
+  def set_homework
+    @homework = Homework.find(params[:homework_id])
+  end
+
+  def set_submission
+    @submission = Submission.find(params[:id])
+  end
 end
