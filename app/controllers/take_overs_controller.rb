701ece321271853edae5230cc27e248458e652a8
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-12-18 17:05:50 +0900

    take_overに編集・更新・削除機能追加

--- app/controllers/take_overs_controller.rb
+++ app/controllers/take_overs_controller.rb
@@ -1,13 +1,15 @@
 class TakeOversController < ApplicationController
   # 学生は全て参照不可
   before_action :authenticate_teacher!
+  before_action :set_classroom, only: [:create, :show, :edit, :update, :destroy]
+  before_action :set_take_over, only: [:show, :edit, :update]
+
 
   def new
     @take_over = TakeOver.new
   end
 
   def create
-    @classroom = Classroom.find(params[:classroom_id])
     @take_over = TakeOver.new(take_over_params)
     if @take_over.valid?
       @take_over.save
@@ -18,7 +20,29 @@ class TakeOversController < ApplicationController
   end
 
   def show
-    @take_over = TakeOver.find(params[:id])
+  end
+
+  def edit
+    if current_teacher.id != @take_over.teacher_id 
+      redirect_to classroom_path(@classroom.id)
+    end
+  end
+
+  def update
+    if @take_over.update(take_over_params)
+      redirect_to classroom_path(@classroom.id)
+    else
+      render "take_overs/edit"
+    end
+  end
+
+  def destroy
+    take_over = TakeOver.find(params[:id])
+    if take_over.destroy
+      redirect_to classroom_path(@classroom.id)
+    else
+      render "take_overs/show"
+    end
   end
 
   private
@@ -27,4 +51,11 @@ class TakeOversController < ApplicationController
     params.require(:take_over).permit(:start_time, :content, :detail, :appearance, :judgement).merge(classroom_id: @classroom.id, teacher_id: current_teacher.id)
   end
 
+  def set_classroom
+    @classroom = Classroom.find(params[:classroom_id])
+  end
+
+  def set_take_over
+    @take_over = TakeOver.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :chatrooms, only: [:new, :create, :show] do
       resources :chats, only: [:create]
     end
-    resources :take_overs, only: [:new, :create, :show]
+    resources :take_overs, except: :index
     resources :homeworks, except: :index do
       resources :submissions, except: :index
     end
