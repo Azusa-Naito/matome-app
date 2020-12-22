4ffb71b78ca85ca795c4cfb5bb42ecfa6ab628fa
Azusa-Naito <69580714+Azusa-Naito@users.noreply.github.com>
2020-12-22 17:28:43 +0900

    学生ゲストログインの記述

--- app/models/student.rb
+++ app/models/student.rb
@@ -18,5 +18,17 @@ class Student < ApplicationRecord
     validates :birthday
   end
 
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |student|
+      student.student_number = Faker::Number.number(digits: 7)
+      student.name = Faker::Name.name
+      student.name_k = Gimei.name.katakana
+      student.nickname = Gimei.name.last.katakana
+      student.image = open('./db/fixtures/student.png')
+      student.email = Faker::Internet.email
+      student.password = SecureRandom.urlsafe_base64
+      student.birthday = Faker::Date.between(from: '1990-01-01', to: '2002-4-01')
+    end
+  end
 end
 
